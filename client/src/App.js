4c637eea1059d4d7204a228628554da7c498af04
waleedrehman31 <waleedrehman31@gmail.com>
2022-12-30 14:55:37 +0500

    :sparkles: setting up layout :sparkles:

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
 import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetail/ItemDetails";
 
 const ScrollTotop = () => {
 	const { pathname } = useLocation();
@@ -19,10 +22,13 @@ const App = () => {
 				<ScrollTotop />
 				<Routes>
 					<Route path="/" element={<Home />} />
+					<Route path="item/:itemId" element={<ItemDetails />} />
+					<Route path="checkout" element={<Checkout />} />
+					<Route path="checkout/success" element={<Confirmation />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
 	);
-}
+};
 
 export default App;
---
+++ client/src/scenes/itemDetail/ItemDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ItemDetails = () => {
+	return <div>ItemDetail</div>;
+};
+
+export default ItemDetails;
